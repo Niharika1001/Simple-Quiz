f470dc0f75ca9e60b0771d3e16d16322f1da2da4
Niharika1001 <thivariniharika@gmail.com>
2024-06-16 15:55:34 +0530

    Quiz

---
+++ quiz1.js
@@ -0,0 +1,13 @@
+function answers()
+{
+    var q=document.getElementById("demo1").innerHTML="Correct Answer: Draupadi Murmu";
+    var q=document.getElementById("demo2").innerHTML=" Correct Answer:Oxygen";
+    var q=document.getElementById("demo3").innerHTML="Correct Answer:Moon of Jupiter";
+    var q=document.getElementById("demo4").innerHTML="Correct Answer:10080";
+    var q=document.getElementById("demo5").innerHTML="Correct Answer:Amazon";
+    var q=document.getElementById("demo6").innerHTML="Correct Answer: Mars";
+    var q=document.getElementById("demo7").innerHTML="Correct Answer:Pacific Ocean";
+    var q=document.getElementById("demo8").innerHTML="Correct Answer: 42";
+    var q=document.getElementById("demo9").innerHTML="Correct Answer:Shakesphere";
+    var q=document.getElementById("demo10").innerHTML="Correct Answer: Height";
+}
